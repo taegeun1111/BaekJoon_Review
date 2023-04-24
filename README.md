12f63017f80450cf4109dc9bde7fdbc8b1e1c790
taegeun1111 <122959190+taegeun1111@users.noreply.github.com>
2023-04-24 23:39:00 +0900

    Review#4_1654

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # BaekJoon_ReviewNote
 ## 10809
 - Ascii코드를 잘 사용하자
+
---
+++ src/오답노트/baekJoon10773.java
@@ -0,0 +1,36 @@
+package 오답노트;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class baekJoon10773 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int top = -1;
+
+        int K = Integer.parseInt(br.readLine());
+        int[] arr = new int[K];
+
+        for (int i = 0; i < K; i++) {
+            int number = Integer.parseInt(br.readLine());
+
+            if (number == 0){
+                top --;
+            }else {
+                top++;
+                arr[top] = number;
+            }
+        }
+        int sum = 0;
+        for (int i = 0; i < top; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println(sum);
+    }
+
+
+
+
+}
